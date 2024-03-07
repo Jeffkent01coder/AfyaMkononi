8e71aa894f43dcdcb796646f0b6af86c9f1adf09
jeff <geoffreyerastus956@gmail.com>
2024-03-06 23:02:08 -0500

    auto login

--- app/src/main/java/com/example/afyamkononi/MainActivity.kt
+++ app/src/main/java/com/example/afyamkononi/MainActivity.kt
@@ -4,19 +4,34 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
+import com.example.afyamkononi.databinding.ActivityMainBinding
 import com.example.afyamkononi.screens.Home
 import com.example.afyamkononi.userAuth.Login
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        supportActionBar?.hide()
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
-        Handler().postDelayed({
-            val intent = Intent(this, Login::class.java)
-            startActivity(intent)
-            finish()
-        },3000)
+        auth = FirebaseAuth.getInstance()
+        Handler(Looper.myLooper()!!).postDelayed(Runnable {
+            if (auth.currentUser != null) {
+                val intent = Intent(this, Home::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                val intent = Intent(this, Login::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }, 2000)
     }
 }
