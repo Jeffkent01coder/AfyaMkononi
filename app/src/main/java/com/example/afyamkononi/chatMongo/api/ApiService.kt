b7255ce1d3fae5774d056144f3d8c15004b9b461
jeff <geoffreyerastus956@gmail.com>
2024-03-13 23:38:44 -0400

    mongochat Fetch

--- app/src/main/java/com/example/afyamkononi/chatMongo/api/ApiService.kt
+++ app/src/main/java/com/example/afyamkononi/chatMongo/api/ApiService.kt
@@ -1,6 +1,7 @@
 package com.example.afyamkononi.chatMongo.api
 
 import com.example.afyamkononi.chatMongo.model.Message
+import com.example.afyamkononi.chatMongo.model.MessageSend
 import com.google.gson.annotations.SerializedName
 import retrofit2.Call
 import retrofit2.http.Body
@@ -11,10 +12,15 @@ import retrofit2.http.Path
 interface ApiService {
 
     @POST("/messages")
-    fun sendMessage(@Body message: Message): Call<ConversationIdResponse> // Modify return type
+    fun sendMessage(
+        @Body message: MessageSend
+    ): Call<ConversationIdResponse>
 
-    @GET("/messages/{id}")
-    fun getMessagesForConversation(@Path("id") id: String): Call<List<Message>>
+    @GET("/messages/{sender}/{receiver}")
+    fun getMessages(
+        @Path("sender") sender: String?,
+        @Path("receiver") receiver: String
+    ): Call<List<Message>>
 }
 
 data class ConversationIdResponse(
