76a450103b7968fdb0ff59230c1bb78150de3a97
jeff <geoffreyerastus956@gmail.com>
2024-03-16 17:58:59 -0400

    Machine learning

---
+++ app/src/main/java/com/example/afyamkononi/machineLearning/PreviousScans.kt
@@ -0,0 +1,12 @@
+package com.example.afyamkononi.machineLearning
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.afyamkononi.R
+
+class PreviousScans : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_previous_scans)
+    }
+}
