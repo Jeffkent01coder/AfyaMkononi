46c681ad638d9669607868bbdfcdab40062e2893
jeff <geoffreyerastus956@gmail.com>
2024-03-19 19:15:15 -0400

    fragreg

--- app/src/main/java/com/example/afyamkononi/exercise/retrofit/ApiExerciseService.kt
+++ app/src/main/java/com/example/afyamkononi/exercise/retrofit/ApiExerciseService.kt
@@ -8,6 +8,8 @@ interface ApiExerciseService{
     @GET("/exercises")
     suspend fun getExercises(
         @Query("limit") limit: Int = 10,
-        @Query("rapidapi-key") apiKey: String = "37464653damsh941e7c9227e0bf9p1b0871jsnbe2ddf38abba"
+        @Query("rapidapi-key") apiKey: String = ""
     ): ApiExerciseResponse
 }
+
+//37464653damsh941e7c9227e0bf9p1b0871jsnbe2ddf38abba
